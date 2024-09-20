ab7bbcbc02c75619c0eab152cadfae6d19d1fda1
lpbluaan <luanb2007247@student.ctu.edu.vn>
2024-09-20 21:00:09 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const contactsRouter = require("./app/routes/contact.route"); // Import route quản lý liên hệ
 
-app.use(cors());
-app.use(express.json());
+const app = express(); // Khởi tạo ứng dụng Express
+
+app.use(cors()); // Sử dụng CORS để xử lý các request từ domain khác
+app.use(express.json()); // Để Express có thể hiểu được JSON
+
+// Sử dụng router cho các route liên hệ
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
