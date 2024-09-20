9f48506b771bfd760ccc6d72103369ce7245f2e4
lpbluaan <luanb2007247@student.ctu.edu.vn>
2024-09-20 21:19:35 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route"); // Import route quản lý liên hệ
+const contactsRouter = require("./app/routes/contact.route"); 
+const ApiError = require("./app/api-error"); 
 
-const app = express(); // Khởi tạo ứng dụng Express
+const app = express(); 
 
-app.use(cors()); // Sử dụng CORS để xử lý các request từ domain khác
-app.use(express.json()); // Để Express có thể hiểu được JSON
+app.use(cors()); 
+app.use(express.json()); 
 
 // Sử dụng router cho các route liên hệ
 app.use("/api/contacts", contactsRouter);
 
+// Handle 404 response
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route nào khớp với yêu cầu
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+    res.json({ message: "Welcome to contact book application." });
 });
 
 module.exports = app;
